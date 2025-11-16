f105f15cfd43a330087860d59e16e25180d8a1c9
Rui Marques <a.marques1967@gmail.com>
2025-11-16 18:21:57 +0000

    fix: update title in App component to remove "React"

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-4">
       <h1 className="text-4xl md:text-5xl font-bold text-slate-100 mb-4 font-sans">
-        React Sudoku
+        Sudoku
       </h1>
       <Game />
     </main>
